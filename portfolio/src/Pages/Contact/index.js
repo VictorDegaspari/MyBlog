c6c6eeccf410a7b68ef17bdcae3f5c6f056e3ee8
Victor Degaspari <vdegaspari.vm@gmail.com>
2020-08-17 12:50:05 -0300

    MyBlog

--- portfolio/src/Pages/Contact/index.js
+++ portfolio/src/Pages/Contact/index.js
@@ -1,12 +1,47 @@
-import React from 'react'
-import './styles.css'
-import MenuVertical from '../../Components/Menu'
- function Contact() {
+import React from "react";
+import "./styles.css";
+import MenuVertical from "../../Components/Menu";
+
+
+function Contact() {
     return (
-        <div>
-             <MenuVertical/>
+        <div className="envolto">
+            <MenuVertical />
+            <header className="header-contact">
+                <h2>Hipster Ipsum</h2>
+                <p></p>
+            </header>
+
+            <section className="grid-section"> 
+                <h1 className="Título">Projetos realizados</h1>
+
+                <iframe className="pinterest-prato"
+                        src="https://assets.pinterest.com/ext/embed.html?id=769974867535330701"
+                        height="220"
+                        width="236"
+                        frameBorder="0"
+                        scrolling="no"
+                        
+                    ></iframe>
+                    <iframe className = "pinterest-peixe"
+                        src="https://assets.pinterest.com/ext/embed.html?id=852587773197050796"
+                        height="270"
+                        width="236"
+                        frameBorder="0"
+                        scrolling="no"
+                    ></iframe>
+                    
+                    <iframe className="pinterest"
+                        src="https://assets.pinterest.com/ext/embed.html?id=769974867535330768"
+                        height="341"
+                        width="236"
+                        frameBorder="none"
+                        scrolling="no"
+                    ></iframe>
+                
+            </section>
         </div>
-    )
+    );
 }
 
 export default Contact;
